ae86abbae25d9e648dad3f907e07fd78581fa953
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2022-04-03 21:27:59 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <Header />
+  <Nav />
+  <div id="content" class="content">
+    <router-view />
+  </div>
 </template>
 
 <script>
 import Header from './components/layouts/Header.vue';
+import Nav from './components/layouts/Nav.vue';
 
 export default {
   name: 'App',
   components: {
     Header,
+    Nav,
   },
 };
 </script>
