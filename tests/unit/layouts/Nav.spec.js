14de2f92556fae67e43bec0bf3c39210221b1656
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2022-04-03 21:54:46 +0900

    Test: "Nav 테스트코드 추가"

--- tests/unit/layouts/Nav.spec.js
+++ tests/unit/layouts/Nav.spec.js
@@ -6,7 +6,13 @@ describe('Header', () => {
   it('renders Nav', () => {
     expect(wrapper.find('[data-test="Nav"]').exists()).toBe(true);
   });
-  it('카테고리가 있다.', () => {
-    expect(wrapper.find('[data-test="nav-category"]').exists()).toBe(true);
+  it('contains 9 locations', () => {
+    const testLocationText = ['카테고리', '이벤트', '베스트', '신작', '혜택관', '우주최저가', '즐기기', '제작의뢰', '입점신청'];
+
+    for (let i = 0; i < testLocationText.length; i += 1) {
+      const localWrapper = wrapper.get(`li:nth-child(${i + 1})`);
+
+      expect(localWrapper.text()).toEqual(testLocationText[i]);
+    }
   });
 });
